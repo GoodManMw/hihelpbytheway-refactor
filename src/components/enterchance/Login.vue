2b1556df525a5d40dfe4517c5885a1b735dca030
GoodMan <15626404376@163.com>
2020-05-17 22:54:28 +0800

    feature: 登录页面第一版

---
+++ src/components/enterchance/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-container>
+    <!-- 登录页面页头区域 -->
+    <el-header>{{ applicationName }}</el-header>
+    <!-- 登录页面主体内容区域 -->
+    <el-main>
+      <!-- 头像昵称区域 -->
+      <div>
+        <el-avatar v-bind:src="userInfo.avatarSrc"></el-avatar>
+        
+      </div>
+    </el-main>
+    <!-- 登录页面页脚区域 -->
+    <el-footer></el-footer>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      applicationName: 'Hi顺带帮帮我',
+      userInfo: {
+        avatarSrc: 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png'
+      }
+    }
+  }
+}
+</script>
